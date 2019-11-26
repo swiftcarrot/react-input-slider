a70590be16237452f6170ff09edf178c945cadf1
Wang Zuo <wzuoadjusted@gmail.com>
2019-11-26 11:44:56 +0800

    add prettier & update snapshot

--- src/__tests__/slider.js
+++ src/__tests__/slider.js
@@ -5,88 +5,88 @@ import Slider from '../slider';
 test('x', () => {
   const component = renderer.create(<Slider />);
   expect(component.toJSON()).toMatchInlineSnapshot(`
-<div
-  className="css-1ik466l-Slider"
-  onClick={[Function]}
->
-  <div
-    className="css-z9sd19-Slider"
-    style={
-      Object {
-        "width": "50%",
-      }
-    }
-  />
-  <div
-    className="css-qnlffc-Slider"
-    onClick={[Function]}
-    onMouseDown={[Function]}
-    onTouchStart={[Function]}
-    style={
-      Object {
-        "left": "50%",
-        "top": "0%",
-      }
-    }
-  />
-</div>
-`);
+    <div
+      className="css-mcm35l"
+      onClick={[Function]}
+    >
+      <div
+        className="css-c5m0sj"
+        style={
+          Object {
+            "width": "50%",
+          }
+        }
+      />
+      <div
+        className="css-1ed5bza"
+        onClick={[Function]}
+        onMouseDown={[Function]}
+        onTouchStart={[Function]}
+        style={
+          Object {
+            "left": "50%",
+            "top": "0%",
+          }
+        }
+      />
+    </div>
+  `);
 });
 
 test('y', () => {
   const component = renderer.create(<Slider axis="y" />);
   expect(component.toJSON()).toMatchInlineSnapshot(`
-<div
-  className="css-16kwc1d-Slider"
-  onClick={[Function]}
->
-  <div
-    className="css-i5fopb-Slider"
-    style={
-      Object {
-        "height": "50%",
-      }
-    }
-  />
-  <div
-    className="css-ftafac-Slider"
-    onClick={[Function]}
-    onMouseDown={[Function]}
-    onTouchStart={[Function]}
-    style={
-      Object {
-        "left": "0%",
-        "top": "50%",
-      }
-    }
-  />
-</div>
-`);
+    <div
+      className="css-1munbi2"
+      onClick={[Function]}
+    >
+      <div
+        className="css-ee7l6q"
+        style={
+          Object {
+            "height": "50%",
+          }
+        }
+      />
+      <div
+        className="css-hpz2p"
+        onClick={[Function]}
+        onMouseDown={[Function]}
+        onTouchStart={[Function]}
+        style={
+          Object {
+            "left": "0%",
+            "top": "50%",
+          }
+        }
+      />
+    </div>
+  `);
 });
 
 test('xy', () => {
   const component = renderer.create(<Slider axis="xy" />);
   expect(component.toJSON()).toMatchInlineSnapshot(`
-<div
-  className="css-8jucwj-Slider"
-  onClick={[Function]}
->
-  <div
-    className="css-1kevm2j-Slider"
-    style={Object {}}
-  />
-  <div
-    className="css-fo2pii-Slider"
-    onClick={[Function]}
-    onMouseDown={[Function]}
-    onTouchStart={[Function]}
-    style={
-      Object {
-        "left": "50%",
-        "top": "50%",
-      }
-    }
-  />
-</div>
-`);
+    <div
+      className="css-1rhaxo2"
+      onClick={[Function]}
+    >
+      <div
+        className="css-0"
+        style={Object {}}
+      />
+      <div
+        className="css-d0dncw"
+        onClick={[Function]}
+        onMouseDown={[Function]}
+        onTouchStart={[Function]}
+        style={
+          Object {
+            "left": "50%",
+            "top": "50%",
+          }
+        }
+      />
+    </div>
+  `);
 });
