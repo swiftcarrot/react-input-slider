4c19f6f2362ae67486c7e5063383c7c30c4fe922
Wang Zuo <wzuoadjusted@gmail.com>
2019-11-26 11:40:36 +0800

    add types

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Interpolation } from '@emotion/serialize';
+
+interface InputSliderProps {
+  axis?: 'x' | 'y' | 'xy';
+  x?: number;
+  xmax?: number;
+  xmin?: number;
+  y?: number;
+  ymax?: number;
+  ymin?: number;
+  xstep?: number;
+  ystep?: number;
+  onChange?: (values: { x: number; y: number }) => void;
+  onDragStart?: (e: MouseEvent) => void;
+  onDragEnd?: (e: MouseEvent) => void;
+  disabled?: boolean;
+  styles?: {
+    track?: Interpolation;
+    active?: Interpolation;
+    thumb?: Interpolation;
+    disabled?: Interpolation;
+  };
+}
+
+export default class Slider extends React.Component<InputSliderProps> {}
